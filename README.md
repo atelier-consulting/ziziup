07f9303032f925e9e2ed75eb09ac24665ae31d51
Serge Tymoshenko <sergiy.tymoshenko@icloud.com>
2016-09-07 17:32:08 +0300

    Boilerplate

---
+++ README.md
@@ -0,0 +1,44 @@
+ziziup-slicing
+===
+
+HTML and CSS for ZIZIUP
+
+### Running
+#### Initial setup
+1. Install Node.js and `npm`.
+1. Clone this repository.
+1. Install dependencies: `npm install`.
+
+#### Runing for development
+`npm run dev`
+
+This opens index.html in default browser and starts watcher, so all changes in
+`src` folder are reflected in that window immediately, without manual page
+reload. If you accidentally close browser window, you can find it at
+[http://localhost:3000](http://localhost:3000)
+
+#### Building for production
+`npm run build`
+
+This places minified styles and images to `prod` folder.
+
+### Conventions
+#### BEM class naming
+We're using three levels deep BEM notation: **Block** => **Element** =>
+**Modifier**. Element separator is double underscore – `__`, modifier separator
+is double dash – `--`, word separator is single dash – `-`.
+
+Example: `block-name__element-name--modifier-name`, `tabbar__tab--active`.
+
+
+We're using multi-class pattern to describe modifications to block or element.
+Example: `<div class="tabbar__tab tabbar__tab--active">...</div>`.
+
+#### Code style
+- 2 spaces indent;
+- Lowercase variables, selectors and attributes;
+- Empty line at the end of file;
+- Comma-delimited selectors on separate line;
+- SASS variable names must be abstracted out of its values (`$color-blue` is
+wrong) and follow `$generic-specific-variation` pattern, e.g.,
+`$color-brand-primary`. Please use dash (`-`) as word separator.
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp');
+var sync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var newer = require('gulp-newer');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var mini = require('gulp-cssmin');
+
+var path = {
+    src: {
+        html: 'src/**/*.html',
+        scss: 'src/sass/**/*.scss',
+        scssPROD: 'src/sass/prod.scss',
+        img : 'src/img/*.*'
+    },
+    dev: {
+        html: 'dev',
+        css: 'dev/css',
+        img: 'dev/img'
+    },
+    prod: {
+        css: 'prod/css',
+        img: 'prod/img'
+    }
+};
+
+var VERSION = process.env.npm_package_version;
+
+gulp.task('serve', ['html', 'sass', 'img'], function(){
+    sync.init({
+        server: './dev'
+    });
+    gulp.watch(path.src.html, ['html']);
+    gulp.watch(path.src.scss, ['sass']);
+    gulp.watch(path.src.img, ['img']);
+});
+
+gulp.task('build', function(){
+    gulp.src(path.src.scssPROD)
+        .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(mini({keepSpecialComments: 0}))
+        .pipe(rename('styles-v' + VERSION +'.min.css'))
+        .pipe(gulp.dest(path.prod.css));
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.prod.img));
+})
+
+gulp.task('sass', function(){
+    return gulp.src(path.src.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dev.css))
+        .pipe(sync.stream());
+});
+
+gulp.task('html', function(){
+    return gulp.src(path.src.html)
+        .pipe(replace('%VERSION%', VERSION))
+        .pipe(gulp.dest(path.dev.html))
+        .pipe(sync.stream());
+});
+
+gulp.task('img', function(){
+    return gulp.src(path.src.img)
+        .pipe(newer(path.dev.img))
+        .pipe(gulp.dest(path.dev.img))
+        .pipe(sync.stream());
+});
+
+gulp.task('default', ['serve']);
