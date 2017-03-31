ecabc796a7b4654a21d105928a1c1a86c5d4305f
Serge Tymoshenko <sergiy.tymoshenko@icloud.com>
2017-03-31 20:32:04 +0300

    Working Pick Up Point

--- src/js/cho.moon.js
+++ src/js/cho.moon.js
@@ -22,21 +22,92 @@ var cho = new Moon({
       path: 'closed', // 'pickup', 'address', 'location', 'open'
       values: [],
       currentValue: false,
+      currentPoint: {
+        id: 0
+      },
       action: 'Add',
       location: {
         city: 'Bamenda',
         country: 'Cameroon'
+        // city: 'Lagos',
+        // country: 'Nigeria'
       },
       points: [{
         id: 1,
+        type: 'pickup',
+        city: 'Bamenda',
+        country: 'Cameroon',
         line1: 'Niki Mash International',
         line2: 'Small Mankon, Bamenda, NWR'
+      },{
+        id: 2,
+        type: 'pickup',
+        city: 'Bamenda',
+        country: 'Cameroon',
+        line1: 'Post Office',
+        line2: 'Ngwafang Street, Bamenda, NWR'
+      },{
+        id: 11,
+        type: 'pickup',
+        city: 'Yaounde',
+        country: 'Cameroon',
+        line1: 'Happy Mall',
+        line2: 'Yaounde, Cameroon'
+      },{
+        id: 101,
+        type: 'pickup',
+        city: 'Accra',
+        country: 'Ghana',
+        line1: 'Stop Point',
+        line2: 'Main Street 67, Accra, Ghana'
       }]
     }
   },
   methods: {
     navigate: function(module, destination) {
       this.set(module + '.path', destination);
+    },
+    selectShippingPoint: function(id) {
+      var point = this.get('shipping').points.filter(function(point) {
+        return point.id === id;
+      })[0];
+      this.set('shipping.currentPoint', point);
+    },
+    unselectShippingPoint: function() {
+      this.set('shipping.currentPoint', {id:0});
+      if (this.get('shipping').values.length) {
+        this.set('shipping.path', 'list');
+      } else {
+        this.set('shipping.path', 'closed');
+      }
+    },
+    selectShippingValue: function(id) {
+      var value = this.get('shipping').values.filter(function(value) {
+        return value.id === id
+      })[0];
+      this.set('shipping.currentValue', value);
+      console.log(this.get('shipping').currentValue);
+    },
+    submitShippingPoint: function(id) {
+      var values = this.get('shipping').values;
+      var newPoint = this.get('shipping').points.filter(function(point) {
+        return point.id === id;
+      })[0];
+      if(values.filter(function(value) {
+        return value.id === newPoint.id;
+      }).length < 1) {
+        values.push(newPoint);
+      };
+      this.set('shipping.values', values);
+      this.set('shipping.currentPoint', {id:0});
+      this.set('shipping.currentValue', newPoint);
+      this.set('shipping.path', 'list');
+    },
+    cancelAddingAddress: function() {
+      this.set('shipping.path', 'closed');
+    },
+    cancelChangingLocation: function() {
+      this.set('shipping.path', 'pickup');
     }
   },
   computed: {
@@ -68,6 +139,35 @@ var cho = new Moon({
       get: function() {
         var l = this.get('shipping').location;
         return l.city + ', ' + l.country
+      }
+    },
+    shippingFilteredPoints: {
+      get: function() {
+        var s = this.get('shipping');
+        var selectedIds = s.values.map(function(value){
+          return value.id;
+        });
+        var notSelectedPoints = s.points.filter(function(point) {
+          var match = selectedIds.filter(function(id){
+            return id === point.id;
+          });
+          return match.length < 1;
+        });
+        var byCity = notSelectedPoints.filter(function(point) {
+          return point.city.toLowerCase() === s.location.city.toLowerCase();
+        });
+        if (byCity.length) {
+          return byCity;
+        } else {
+          var byCountry = notSelectedPoints.filter(function(point) {
+            return point.country.toLowerCase() === s.location.country.toLowerCase()
+          });
+          if (byCountry.length) {
+            return byCountry;
+          } else {
+            return false;
+          }
+        }
       }
     }
   }
